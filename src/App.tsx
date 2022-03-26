859efd0d7340377be64d4eeebe398617b8eb548e
Banan-dot <semke.danya@mail.ru>
2022-03-26 23:59:10 +0500

    add common user information

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-import React, { useState } from 'react';
-import UserInfo from './UserInfo/UserInfo';
+import React, { useState } from "react";
+import UserInfo from "./UserInfo/UserInfo";
 
 export default function App(): JSX.Element {
-    const [username, setUsername] = useState<string>('');
-    const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
+  const [username, setUsername] = useState<string>("");
+  const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
 
-    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        setIsSubmitted(true);
-    };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsSubmitted(true);
+  };
 
-    return (
-        <div>
-            {!isSubmitted && (
-                <form onSubmit={onSubmit}>
-                    <label>
-                        Username:
-                        <input
-                            type="text"
-                            value={username}
-                            onChange={e => {
-                                setUsername(e.target.value);
-                                if (isSubmitted) setIsSubmitted(false);
-                            }}
-                        />
-                    </label>
-                    <input type="submit" value="Submit" />
-                </form>
-            )}
-            {isSubmitted && <UserInfo username={username} />}
-        </div>
-    );
+  return (
+    <div>
+      {!isSubmitted && (
+        <form onSubmit={onSubmit}>
+          <label>
+            Username:
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+                if (isSubmitted) setIsSubmitted(false);
+              }}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      )}
+      {isSubmitted && <UserInfo username={username} />}
+    </div>
+  );
 }
--- src/UserInfo/UserInfo.tsx
+++ src/UserInfo/UserInfo.tsx
@@ -1,82 +1,45 @@
-import React from 'react';
-import gql from 'graphql-tag';
-
-import { useQuery } from 'react-apollo';
-
-const organizationByUsernameQuery = `
-      organization(login: $login) {
-        id
-        name
-      }
-`;
-
-const languagesQuery = `
-      languages(first: 5, orderBy: {field: SIZE, direction: ASC}) {
-        edges {
-          LanguageSize: size
-          node {
-            LanguageName: name
-          }
-        }
-      }
-`;
-
-const repositoriesByUsernameQuery = `
-      repositories(first: 4, orderBy: {field: STARGAZERS, direction: DESC}) {
-        nodes {
-          name
-          forkCount
-          url
-          sshUrl
-          stargazerCount
-
-          ${languagesQuery}
-        }
-      }
-`;
-// collaborators {
-//   totalCount
-//   nodes {
-//     login
-//   }
-// }
-const userInfoByUsernameQuery = `
-      repositoryOwner(login: $login) {
-        ... on User{
-            login
-          name
-          email
-          location
-          avatarUrl
-          websiteUrl
-          ${organizationByUsernameQuery}
-          ${repositoriesByUsernameQuery}
-        }
-      }
-`;
-
-const getGithubInformationByUsernameQuery = () =>
-    gql(String.raw`
-        query Login($login: String!) {
-          ${userInfoByUsernameQuery}
-        }
-  `);
+import React from "react";
+import { useQuery } from "react-apollo";
+import { getGithubInformationByUsernameQuery } from "../utils/graphiqlQuries";
 
 type UserInfoProps = {
-    username: string;
+  username: string;
 };
 
-export default function UserInfo({ username }: UserInfoProps) {
-    const { loading, data, error } = useQuery(getGithubInformationByUsernameQuery(), {
-        variables: { login: username }
-    });
+function getCommonInfo(data: any) {
+    const owner = data["repositoryOwner"];
+    const name = owner["name"];
+    const login = owner["login"];
+    const organization = owner["organization"];
+    const email = owner["email"];
+    const websiteUrl = owner["websiteUrl"];
+    const location = owner["location"];
+    const avatarUrl = owner["avatarUrl"];
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <div>Error {error.message}</div>;
-    if (data["repositoryOwner"] !== null) return (
-        <div>
-            <pre>{JSON.stringify(data, null, 2)}</pre>
+    return (
+        <div className="common-user-info">
+            <h3>Common user information:</h3>
+            {avatarUrl && <img src={avatarUrl} alt="userAvatar" width={200} height={200}/>}
+            {name && <p>Name: {owner["name"]}</p>}
+            {login && <p>Login: {owner["login"]}</p>}
+            {organization && <p>Organization: {owner["organization"]}</p>}
+            {email && <p>Email: {owner["email"]}</p>}
+            {websiteUrl && <p>Website: {owner["websiteUrl"]}</p>}
+            {location && <p>Location: {owner["location"]}</p>}
         </div>
     );
-    return null;
+}
+
+export default function UserInfo({ username }: UserInfoProps) {
+  const { loading, data, error } = useQuery(
+    getGithubInformationByUsernameQuery(),
+    {
+      variables: { login: username },
+    }
+  );
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <div>Error {error.message}</div>;
+  if (data["repositoryOwner"] !== null) return <div>{getCommonInfo(data)}</div>;
+  return null;
 }
