5226f84d0fe1809efc1d2968547591578e6b93f4
Banan-dot <semke.danya@mail.ru>
2022-03-28 21:06:03 +0500

    Refactor

--- src/UserInfo/UserInfo.tsx
+++ src/UserInfo/UserInfo.tsx
@@ -1,27 +1,26 @@
 import React from "react";
 import { useQuery } from "react-apollo";
 import { getGithubInformationByUsernameQuery } from "../utils/graphiqlQuries";
-import {getStatistics} from "../utils/statistic";
+import { getStatistics } from "../utils/statistic";
 
 function getCommonInfo(data: any) {
   const owner = data["repositoryOwner"];
 
   return (
-      <div className="common-user-info">
-        <h3>Common user information:</h3>
-        {owner.avatarUrl && (
-            <img src={owner.avatarUrl} alt="userAvatar" width={200} height={200} />
-        )}
-        {owner.name && <p>Name: {owner.name}</p>}
-        {owner.login && <p>Login: {owner.login}</p>}
-        {owner.organization && <p>Organization: {owner.organization}</p>}
-        {owner.email && <p>Email: {owner.email}</p>}
-        {owner.websiteUrl && <p>Website: {owner.websiteUrl}</p>}
-        {owner.location && <p>Location: {owner.location}</p>}
-      </div>
+    <div className="common-user-info">
+      <h3>Common user information:</h3>
+      {owner.avatarUrl && (
+        <img src={owner.avatarUrl} alt="userAvatar" width={200} height={200} />
+      )}
+      {owner.name && <p>Name: {owner.name}</p>}
+      {owner.login && <p>Login: {owner.login}</p>}
+      {owner.organization && <p>Organization: {owner.organization}</p>}
+      {owner.email && <p>Email: {owner.email}</p>}
+      {owner.websiteUrl && <p>Website: {owner.websiteUrl}</p>}
+      {owner.location && <p>Location: {owner.location}</p>}
+    </div>
   );
 }
-
 
 type UserInfoProps = {
   username: string;
