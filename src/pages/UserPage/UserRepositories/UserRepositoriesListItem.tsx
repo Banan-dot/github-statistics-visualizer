e4246dc243249be768bdc169018cb21024851cc0
Semyon Shchelkov <semen2k1@mail.ru>
2022-04-05 18:16:10 +0500

    Add user repositories styles

--- src/pages/UserPage/UserRepositories/UserRepositoriesListItem.tsx
+++ src/pages/UserPage/UserRepositories/UserRepositoriesListItem.tsx
@@ -1,25 +1,49 @@
 import React from "react";
 import Repository from "../../../models/Repository";
+import { RepoForkedIcon, StarIcon } from "@primer/octicons-react";
+import IconDataLabel from "../../../shared/IconDataLabel";
+import LanguageLabel from "../../../shared/LanguageLabel";
+import { Link } from "@skbkontur/react-ui";
 
 type Props = {
   repository: Repository;
 };
 
 const UserRepositoriesListItem = ({ repository }: Props) => {
-  const { owner, primaryLanguage } = repository;
+  const { name, forkCount, stargazerCount, primaryLanguage, owner } =
+    repository;
+
+  const repositoryInfoLink = `user/${owner.login}/repository/${repository.name}`;
 
   return (
-    <div>
-      <hr />
-      <div>Название: {repository.name}</div>
-      <div>Владелец: {owner.login}</div>
+    <li className="user-repository user-repository-list__item">
+      <Link className="user-repository__name-link" href={repositoryInfoLink}>
+        {name}
+      </Link>
+      <div className="user-repository__label-list">
+        <LanguageLabel
+          name={primaryLanguage?.name}
+          color={primaryLanguage?.color}
+        />
+        <IconDataLabel
+          icon={RepoForkedIcon}
+          value={forkCount}
+          hintText="Количество форков"
+        />
+        <IconDataLabel
+          icon={StarIcon}
+          value={stargazerCount}
+          hintText="Количество звезд"
+        />
+      </div>
+      {/* <div>Владелец: {owner.login}</div>
       <div>Основной язык: {primaryLanguage?.name ?? "Нет данных"}</div>
       <div>Количество форков: {repository.forkCount}</div>
       <div>Форк: {repository.isFork ? "Да" : "Нет"}</div>
       <a href={repository.url} target="_blank" rel="noreferrer">
         Открыть на GitHub
-      </a>
-    </div>
+      </a> */}
+    </li>
   );
 };
 
