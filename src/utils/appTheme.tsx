eb5f4e8a48f0f62117b407c119b26cef94fce8a9
Semyon Shchelkov <semen2k1@mail.ru>
2022-04-06 11:59:22 +0500

    Add inputBorderRadius

--- src/utils/appTheme.tsx
+++ src/utils/appTheme.tsx
@@ -4,6 +4,9 @@ const themeOverrides = {
   btnBorderRadiusLarge: "6px",
   btnBorderRadiusMedium: "6px",
   btnBorderRadiusSmall: "6px",
+  inputBorderRadiusSmall: "6px",
+  inputBorderRadiusMedium: "6px",
+  inputBorderRadiusLarge: "6px",
 };
 
 export default ThemeFactory.create(themeOverrides, FLAT_THEME_8PX);
