67f76905997c8942edfcd129cc019a6b4547dad5
Semyon Shchelkov <semen2k1@mail.ru>
2022-04-18 13:46:28 +0500

    Add action buttons

--- src/pages/RepositoryPage/RepositoryInfo.tsx
+++ src/pages/RepositoryPage/RepositoryInfo.tsx
@@ -3,10 +3,11 @@ import { gql, useQuery } from "@apollo/client";
 import Repository from "../../models/Repository";
 
 import PageCard from "../../shared/PageCard";
-import { Link, Spinner, Textarea } from "@skbkontur/react-ui";
+import { Button, Link, Spinner, Textarea } from "@skbkontur/react-ui";
 import Alert from "../../shared/Alert";
 import DataLabel from "../../shared/DataLabel";
 import UserAvatar from "../../shared/UserAvatar";
+import CloneRepositoryButton from "../../shared/CloneRepositoryButton";
 
 const GET_REPOSITORY = gql`
   query GetRepository($login: String!, $repositoryName: String!) {
@@ -25,6 +26,8 @@ const GET_REPOSITORY = gql`
       issues {
         totalCount
       }
+      url
+      sshUrl
       createdAt
       updatedAt
       description
@@ -75,23 +78,37 @@ const RepositoryInfo = ({ login, repositoryName }: Props) => {
       className="repository-page__common-info repository-page__section"
     >
       {data?.repository && (
-        <PageCard.Header>
-          <PageCard.Title className="common-info__title">
-            {data.repository.name}
-          </PageCard.Title>
-          <Link
-            className="user-link"
-            href={`/user/${data.repository.owner.login}`}
-          >
-            <UserAvatar
-              className="user-link__avatar"
-              src={data.repository.owner.avatarUrl}
-              size={24}
+        <PageCard.Header className="repository-common-info__header">
+          <div className="repository-common-info__header-info">
+            <PageCard.Title>{data.repository.name}</PageCard.Title>
+            <Link
+              className="repository-common-info__user-link"
+              href={`/user/${data.repository.owner.login}`}
+            >
+              <UserAvatar
+                className="repository-common-info__user-link-avatar"
+                src={data.repository.owner.avatarUrl}
+                size={24}
+              />
+              <span className="repository-common-info__user-link-login">
+                {data.repository.owner.login}
+              </span>
+            </Link>
+          </div>
+          <div className="repository-common-info__header-action-buttons">
+            <Link
+              className="repository-common-info__header-action-button"
+              href={data.repository.url}
+              target="_blank"
+            >
+              <Button width="100%">Открыть на Github</Button>
+            </Link>
+            <CloneRepositoryButton
+              className="repository-common-info__header-action-button"
+              url={data.repository.url}
+              sshUrl={data.repository.sshUrl}
             />
-            <span className="user-link__login">
-              {data.repository.owner.login}
-            </span>
-          </Link>
+          </div>
         </PageCard.Header>
       )}
 
@@ -110,18 +127,19 @@ const RepositoryInfo = ({ login, repositoryName }: Props) => {
         {data?.repository && (
           <div>
             {data.repository.description && (
-              <div className="description">
+              <div className="repository-common-info__description">
                 <div>Описание</div>
                 <Textarea
                   defaultValue={data.repository.description}
                   width="100%"
                   resize="none"
+                  style={{ minWidth: "none" }}
                   readOnly
                 />
               </div>
             )}
 
-            <div className="data-label-list">
+            <div className="repository-common-info__data-label-list">
               <DataLabel
                 value={
                   data.repository.defaultBranchRef.target.history.totalCount
