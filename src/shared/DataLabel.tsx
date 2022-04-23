c677a7026f550a4bedd2c27d5e680d435aacebea
syomcha <semen2k1@mail.ru>
2022-04-23 15:26:39 +0500

    Add DataLabel value format

--- src/shared/DataLabel.tsx
+++ src/shared/DataLabel.tsx
@@ -1,5 +1,6 @@
 import { Icon } from "@primer/octicons-react";
 import React from "react";
+import { formatToLocaleString } from "../utils/number.helper";
 
 type Props = {
   children?: Array<JSX.Element>;
@@ -7,6 +8,13 @@ type Props = {
   icon?: Icon;
   value: number | string | null | undefined;
   caption: string;
+};
+
+const formatValue = (value: string | number | null | undefined) => {
+  if (value) {
+    return typeof value === "number" ? formatToLocaleString(value, 0) : value;
+  }
+  return "-";
 };
 
 const DataLabel = ({
@@ -20,7 +28,7 @@ const DataLabel = ({
     <div className={`data-label ${className ?? ""}`} title={caption}>
       <div className="data-label__value-container">
         {Icon && <Icon className="data-label__value-icon" size={20} />}
-        <div className="data-label__value">{value ?? "-"}</div>
+        <div className="data-label__value">{formatValue(value)}</div>
       </div>
       <div className="data-label__caption">{caption}</div>
       {children}
