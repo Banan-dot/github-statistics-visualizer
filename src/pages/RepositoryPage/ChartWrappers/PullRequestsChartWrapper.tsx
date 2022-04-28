18c80b1349a10e0fa0fad500023a91128b074eeb
syomcha <semen2k1@mail.ru>
2022-04-28 12:02:55 +0500

    Add labels to repository page about issues and prs & fix chart size

--- src/pages/RepositoryPage/ChartWrappers/PullRequestsChartWrapper.tsx
+++ src/pages/RepositoryPage/ChartWrappers/PullRequestsChartWrapper.tsx
@@ -6,6 +6,12 @@ import Alert from "../../../shared/Alert";
 import PullRequestChart from "../../../shared/charts/PullRequestChart";
 import { RepositoryData, RepositoryVars } from "../../../types/QueryTypes";
 import { RepositoryChartWrapperProps } from "../index";
+import IconDataLabel from "../../../shared/IconDataLabel";
+import {
+  GitMergeIcon,
+  GitPullRequestClosedIcon,
+  GitPullRequestIcon,
+} from "@primer/octicons-react";
 
 const getPullRequestsQuery = (name: string, state: PullRequestState) => gql`
   query ${name} ($login: String!, $repositoryName: String!) {
@@ -52,6 +58,12 @@ const PullRequestsChartWrapper = ({
     closedPullRequests.error ||
     mergedPullRequests.error;
 
+  const pullRequestsInfo = {
+    OPEN: openPullRequests.data?.repository.pullRequests.totalCount ?? 0,
+    CLOSED: closedPullRequests.data?.repository.pullRequests.totalCount ?? 0,
+    MERGED: mergedPullRequests.data?.repository.pullRequests.totalCount ?? 0,
+  };
+
   return (
     <div className={className}>
       {loading && (
@@ -64,16 +76,26 @@ const PullRequestsChartWrapper = ({
       {error && <Alert type="danger">Ошибка загрузки пулл реквестов</Alert>}
 
       {!loading && (
-        <PullRequestChart
-          pullRequestsInfo={{
-            OPEN:
-              openPullRequests.data?.repository.pullRequests.totalCount ?? 0,
-            CLOSED:
-              closedPullRequests.data?.repository.pullRequests.totalCount ?? 0,
-            MERGED:
-              mergedPullRequests.data?.repository.pullRequests.totalCount ?? 0,
-          }}
-        />
+        <div className="pull-request-chart__wrapper">
+          <div className="pull-request-chart__label-list">
+            <IconDataLabel
+              icon={GitPullRequestIcon}
+              value={pullRequestsInfo.OPEN}
+              hintText="Открытые пулл ревквесты"
+            />
+            <IconDataLabel
+              icon={GitMergeIcon}
+              value={pullRequestsInfo.MERGED}
+              hintText="Слитые пулл ревквесты"
+            />
+            <IconDataLabel
+              icon={GitPullRequestClosedIcon}
+              value={pullRequestsInfo.CLOSED}
+              hintText="Закрытые пулл ревквесты"
+            />
+          </div>
+          <PullRequestChart pullRequestsInfo={pullRequestsInfo} />
+        </div>
       )}
     </div>
   );
